57e9725ceb224b959b27e8f7ac979905b3d7066c
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2020-12-07 23:16:15 -0800

    fix common.mk

--- common.mk
+++ common.mk
@@ -1,5 +1,10 @@
 SHELL=/bin/bash
 
-ifndef GOLLY_PELICAN_HOME
-$(error Please run "source environment" in the golly-pelican repo root directory before running make commands)
+ifeq ($(shell echo ${GOLLY_PELICAN_HOME}),)
+$(error Environment variable GOLLY_PELICAN_HOME not defined. Please run "source environment" in the golly-pelican repo root directory before running make commands)
 endif
+
+#ifeq ($(shell which jq),)
+#$(error Please install jq using "apt-get install jq" or "brew install jq")
+#endif
+
